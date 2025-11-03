78548cd8b94e40249655dd166b0b2ad7ae7c615e
Shivam020202 <charlieshivam70@gmail.com>
2025-11-03 12:04:33 +0530

    new updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,18 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 w-full bg-[#FFF8F3] shadow-sm">
-      <div className="max-w-7xl mx-auto px-4 md:px-0 py-5">
+      <div className="max-w-7xl mx-auto px-4 md:px-0 py-2">
         <div className="flex items-center justify-between">
           {/* Logo */}
           <div
             className="text-2xl md:text-3xl font-bold tracking-wide"
             style={{ color: "#E89161" }}
           >
-            LEAP LADDERS
+            <img
+              className="w-16 h-auto"
+              src="leap-ladders-logo.png"
+              alt="Leap Ladders Logo"
+            />
           </div>
 
           {/* Navigation Links - Hidden on mobile */}
